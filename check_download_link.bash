4802d88597370b5f04b14f9fcec02676812a38b8
unsoop <31593022+unsoop@users.noreply.github.com>
2018-09-18 17:05:08 +0300

    changed file exe destination

--- check_download_link.bash
+++ check_download_link.bash
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 
-wget -O <output/file> $1 &> <output/file/print-out>
+wget -O /dev/null $1 &> <output/file/print-out>
 
 if grep -q "100%" /<output/file/print-out>; then
         echo "OK"
-        rm -f <output/file>
         exit 0
 
         elif grep -q "ERROR 404" <output/file/print-out>; then
                 echo "CRITICAL"
-                rm -f <output/file>
                 exit 2
 
         else
                 echo "UNKNOWN"
-                rm -f <output/file>
                 exit 3
 
 fi
