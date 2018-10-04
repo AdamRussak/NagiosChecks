41fd0966ef025c4959cbf87fff900b67fc1af768
unsoop <31593022+unsoop@users.noreply.github.com>
2018-10-04 18:30:24 +0300

    change to wget --spider
    change to wget --spider so all wget process is done but no download. (to conserve data and bandwidth)

--- check_download_link.bash
+++ check_download_link.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 #                           !!!!!!!!! writing by: Adam Russak !!!!!!!!!
 #Command to Download a Using WGET with Accepting $1 and Outpot of data to /dev/null and Wget screen outpot to <output/file/print-out>
-wget -O /dev/null $1 &> <output/file/print-out>
+wget --spider $1 &> <output/file/print-out>
 
 #if Download is Sucessfull the script Exits with Exit Code 0 if there is an error Exit Code 2 & if UNKOWN Exit Code 3
-if grep -q "100%" /<output/file/print-out>; then
+if grep -q "200" /<output/file/print-out>; then
         echo "OK"
         exit 0
 
